5f63b2cc331721a8fba6ed2dc5d4090d9cacc2c7
caiodsc <caio.dscamara@gmail.com>
2018-07-16 10:22:50 -0400

    Custom scaffold for file_uploads

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  #get 'file_uploads/new'
   resources :buckets
   resources :posts
   root to: "buckets#index"
+  resources :buckets do
+    resources :file_uploads, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
