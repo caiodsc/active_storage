c641035f47d49c7b068e6a0fd5ab4c9c765becd3
caiodsc <caio.dscamara@gmail.com>
2018-07-16 09:23:41 -0400

    Add image to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+  has_one_attached :image
+  include Rails.application.routes.url_helpers
+  def image_url
+    rails_blob_path(self.image)#, disposition: "attachment", only_path: true)
+  end
 end
