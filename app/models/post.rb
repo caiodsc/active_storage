a63ce0e8cf01ae610ac01f3210ee2eb9cc10ea54
caiodsc <caio.dscamara@gmail.com>
2018-07-16 09:58:12 -0400

    Model bucket and add polymorphic url to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   has_one_attached :image
   include Rails.application.routes.url_helpers
   def image_url
-    rails_blob_path(self.image)#, disposition: "attachment", only_path: true)
+    #rails_blob_path(self.image)#, disposition: "attachment", only_path: true) #.variant(resize: '140x140')
+    polymorphic_url self.image, only_path: true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :buckets
   resources :posts
+  root to: "buckets#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
